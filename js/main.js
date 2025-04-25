980589254c856b5c814fa609fceb4079f543458d
oleg_solovev_2 <leg2012@bk.ru>
2025-04-25 17:49:34 +0300

    #16 Добавление библиотеки jQuery и карточки стека SASS, изменение названий разделов и описания к проектам

--- js/main.js
+++ js/main.js
@@ -1,15 +1,38 @@
 // События кликов для бургер меню
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("burger").addEventListener("click", function () {
-        document.querySelector("header").classList.toggle("open")
-    })
-    let el = document.querySelectorAll('.nav__link');
-    for (let i = 0; i < el.length; i++) {
-        el[i].addEventListener("click", function () {
-            document.querySelector("header").classList.toggle("open")
+$(document).ready(
+    document.addEventListener("DOMContentLoaded", function () {
+        $('#burger').on("click", function () {
+            $('.header').toggleClass("open");
         })
-    }
-})
+        const el = $('.nav__link');
+        for (let i = 0; i < el.length; i++) {
+            console.log(el[i])
+            el[i].addEventListener("click", function () {
+                $('.header').toggleClass("open");
+            })
+        }
+        $(document).on("click", function (event) {
+            const targetElement = $('#burger');
+            if (!targetElement.is(event.target) && targetElement.has(event.target).length === 0) {
+                $('.header').toggleClass("open");
+            }
+        })
+    })
+)
+
+
+// События кликов для бургер меню
+// document.addEventListener("DOMContentLoaded", function () {
+//     document.getElementById("burger").addEventListener("click", function () {
+//         document.querySelector("header").classList.toggle("open")
+//     })
+//     let el = document.querySelectorAll('.nav__link');
+//     for (let i = 0; i < el.length; i++) {
+//         el[i].addEventListener("click", function () {
+//             document.querySelector("header").classList.toggle("open")
+//         })
+//     }
+// })
 
 // Галерея
 let swiper = new Swiper(".gallery-swiper", {
